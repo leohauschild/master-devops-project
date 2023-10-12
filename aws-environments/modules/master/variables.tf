147ed8e012bf671ea01f73bcdd267a7c3e7a5d18
Leonardo Hauschild <leonardo@khomp.com>
2023-10-12 15:59:35 -0300

    feat(modules): Create module eks master
    Create eks module to facilitate future deployments and to better organize the structure of terraform codes

---
+++ aws-environments/modules/master/variables.tf
@@ -0,0 +1,11 @@
+variable "project_name" {}
+
+variable "aws_region" {}
+
+variable "cluster_version" {}
+
+variable "cluster_vpc" {}
+
+variable "private_subnet_1a" {}
+
+variable "private_subnet_1b" {}
--- aws-environments/terrafile.tf
+++ aws-environments/terrafile.tf
@@ -1,6 +1,18 @@
-module "vpc" {
-  source        = "./modules/network"
-  
-  project_name  = var.project_name
-  aws_region    = var.aws_region
+module "network" {
+  source = "./modules/network"
+
+  project_name = var.project_name
+  aws_region   = var.aws_region
+}
+
+module "eks_master" {
+  source = "./modules/master"
+
+  project_name    = var.project_name
+  aws_region      = var.aws_region
+  cluster_version = var.cluster_version
+
+  cluster_vpc       = module.network.project_vpc
+  private_subnet_1a = module.network.private_subnet_1a
+  private_subnet_1b = module.network.private_subnet_1b
 }
